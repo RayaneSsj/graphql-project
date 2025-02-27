a343d38e70d592006a3b5601c673b6517e59bd62
kevin <kcm.parry@gmail.com>
2025-02-27 10:47:28 +0100

    Modification du resolver pour le user (placé dans un fichier à part)

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,15 +1,9 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { userResolvers } from './revolvers/user.resolver';
 
 dotenv.config();
-
-const JWT_SECRET = process.env.JWT_SECRET || 'fallback-secret';
-
-const prisma = new PrismaClient();
 
 const typeDefs = `
   type User {
@@ -28,30 +22,13 @@ const typeDefs = `
   }
 `;
 
+
 const resolvers = {
   Query: {
-    users: async () => await prisma.user.findMany(),
+    ...userResolvers.Query,
   },
   Mutation: {
-    signUp: async (_: unknown, { name, email, password }: { name: string; email: string; password: string }) => {
-      const existingUser = await prisma.user.findUnique({ where: { email } });
-      if (existingUser) {
-        throw new Error('Email already in use');
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const user = await prisma.user.create({
-        data: { name, email, password: hashedPassword },
-      });
-      return jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '7d' });
-    },
-    signIn: async (_: unknown, { email, password }: { email: string; password: string }) => {
-      const user = await prisma.user.findUnique({ where: { email } });
-      if (!user || !(await bcrypt.compare(password, user.password))) {
-        throw new Error('Invalid credentials');
-      }
-      return jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '7d' });
-    },
+    ...userResolvers.Mutation,
   },
 };
 
