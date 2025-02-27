03caf8f1bb4519ad75a6a93ae1542f7995a43f4a
kevin <kcm.parry@gmail.com>
2025-02-27 12:48:05 +0100

    Mise en place de la gestion des articles (get, post, update, delete)

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import dotenv from 'dotenv';
 import { userResolvers } from './revolvers/user.resolver';
+import { articleResolvers } from './revolvers/article.resolver';
 
 dotenv.config();
 
@@ -12,23 +13,36 @@ const typeDefs = `
     email: String!
   }
 
+  type Article {
+    id: ID!
+    title: String!
+    content: String!
+    authorId: String!
+  }
+
   type Query {
     users: [User!]
+    articles: [Article!]
+    article(id: ID!): Article
   }
 
   type Mutation {
     signUp(name: String!, email: String!, password: String!): String
     signIn(email: String!, password: String!): String
+    createArticle(title: String!, content: String!, authorId: String!): Article
+    updateArticle(id: ID!, title: String, content: String): Article
+    deleteArticle(id: ID!): String
   }
 `;
-
 
 const resolvers = {
   Query: {
     ...userResolvers.Query,
+    ...articleResolvers.Query,
   },
   Mutation: {
     ...userResolvers.Mutation,
+    ...articleResolvers.Mutation,
   },
 };
 
